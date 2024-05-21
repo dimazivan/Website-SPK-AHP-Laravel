55e128cea7b98a01639f31dbe14c0394b99e1954
dimazivan <dimazivan740@gmail.com>
2024-05-21 21:49:20 +0700

    new db

--- db_spk.sql
+++ db_spk.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Bulan Mei 2022 pada 20.18
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 8.0.2
+-- Generation Time: May 21, 2024 at 04:48 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,27 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `alternatives`
+-- Table structure for table `alternatives`
 --
 
 CREATE TABLE `alternatives` (
   `id` int(10) UNSIGNED NOT NULL,
-  `tipe` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lantai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kamar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `luas` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `harga` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `garasi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `keterangan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `gambar_denah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipe` varchar(255) NOT NULL,
+  `lantai` varchar(255) NOT NULL,
+  `kamar` varchar(255) NOT NULL,
+  `luas` varchar(255) NOT NULL,
+  `harga` varchar(255) NOT NULL,
+  `garasi` varchar(255) NOT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `gambar_denah` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `alternatives`
+-- Dumping data for table `alternatives`
 --
 
 INSERT INTO `alternatives` (`id`, `tipe`, `lantai`, `kamar`, `luas`, `harga`, `garasi`, `keterangan`, `gambar`, `gambar_denah`, `created_at`, `updated_at`, `deleted_at`) VALUES
@@ -58,23 +58,23 @@ INSERT INTO `alternatives` (`id`, `tipe`, `lantai`, `kamar`, `luas`, `harga`, `g
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `comparisons`
+-- Table structure for table `comparisons`
 --
 
 CREATE TABLE `comparisons` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `tipe` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lantai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kamar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `luas` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `harga` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `garasi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipe` varchar(255) NOT NULL,
+  `lantai` varchar(255) NOT NULL,
+  `kamar` varchar(255) NOT NULL,
+  `luas` varchar(255) NOT NULL,
+  `harga` varchar(255) NOT NULL,
+  `garasi` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `comparisons`
+-- Dumping data for table `comparisons`
 --
 
 INSERT INTO `comparisons` (`id`, `tipe`, `lantai`, `kamar`, `luas`, `harga`, `garasi`, `created_at`, `updated_at`) VALUES
@@ -88,20 +88,20 @@ INSERT INTO `comparisons` (`id`, `tipe`, `lantai`, `kamar`, `luas`, `harga`, `ga
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `criterias`
+-- Table structure for table `criterias`
 --
 
 CREATE TABLE `criterias` (
   `id` int(10) UNSIGNED NOT NULL,
-  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `criterias`
+-- Dumping data for table `criterias`
 --
 
 INSERT INTO `criterias` (`id`, `code`, `name`, `created_at`, `updated_at`, `deleted_at`) VALUES
@@ -114,41 +114,41 @@ INSERT INTO `criterias` (`id`, `code`, `name`, `created_at`, `updated_at`, `dele
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `failed_jobs`
+-- Table structure for table `failed_jobs`
 --
 
 CREATE TABLE `failed_jobs` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `hasils`
+-- Table structure for table `hasils`
 --
 
 CREATE TABLE `hasils` (
   `id` int(10) UNSIGNED NOT NULL,
-  `tipe` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lantai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kamar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `luas` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `harga` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `garasi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipe` varchar(255) NOT NULL,
+  `lantai` varchar(255) NOT NULL,
+  `kamar` varchar(255) NOT NULL,
+  `luas` varchar(255) NOT NULL,
+  `harga` varchar(255) NOT NULL,
+  `garasi` varchar(255) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
   `ahp` double(8,2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `hasils`
+-- Dumping data for table `hasils`
 --
 
 INSERT INTO `hasils` (`id`, `tipe`, `lantai`, `kamar`, `luas`, `harga`, `garasi`, `gambar`, `ahp`, `created_at`, `updated_at`) VALUES
@@ -162,17 +162,17 @@ INSERT INTO `hasils` (`id`, `tipe`, `lantai`, `kamar`, `luas`, `harga`, `garasi`
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `migrations`
+-- Table structure for table `migrations`
 --
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `migrations`
+-- Dumping data for table `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -191,33 +191,33 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `password_resets`
+-- Table structure for table `password_resets`
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `sub_criterias`
+-- Table structure for table `sub_criterias`
 --
 
 CREATE TABLE `sub_criterias` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nilaik` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nilaib` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `nilaik` varchar(255) NOT NULL,
+  `nilaib` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `sub_criterias`
+-- Dumping data for table `sub_criterias`
 --
 
 INSERT INTO `sub_criterias` (`id`, `code`, `name`, `nilaik`, `nilaib`, `created_at`, `updated_at`) VALUES
@@ -239,23 +239,23 @@ INSERT INTO `sub_criterias` (`id`, `code`, `name`, `nilaik`, `nilaib`, `created_
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `role`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
@@ -270,130 +270,130 @@ INSERT INTO `users` (`id`, `name`, `role`, `email`, `email_verified_at`, `passwo
 --
 
 --
--- Indeks untuk tabel `alternatives`
+-- Indexes for table `alternatives`
 --
 ALTER TABLE `alternatives`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `alternatives_tipe_unique` (`tipe`);
 
 --
--- Indeks untuk tabel `comparisons`
+-- Indexes for table `comparisons`
 --
 ALTER TABLE `comparisons`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `comparisons_tipe_unique` (`tipe`);
 
 --
--- Indeks untuk tabel `criterias`
+-- Indexes for table `criterias`
 --
 ALTER TABLE `criterias`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `criterias_code_unique` (`code`);
 
 --
--- Indeks untuk tabel `failed_jobs`
+-- Indexes for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
--- Indeks untuk tabel `hasils`
+-- Indexes for table `hasils`
 --
 ALTER TABLE `hasils`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `hasils_tipe_unique` (`tipe`);
 
 --
--- Indeks untuk tabel `migrations`
+-- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `password_resets`
+-- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
--- Indeks untuk tabel `sub_criterias`
+-- Indexes for table `sub_criterias`
 --
 ALTER TABLE `sub_criterias`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `sub_criterias_code_unique` (`code`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `alternatives`
+-- AUTO_INCREMENT for table `alternatives`
 --
 ALTER TABLE `alternatives`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- AUTO_INCREMENT untuk tabel `comparisons`
+-- AUTO_INCREMENT for table `comparisons`
 --
 ALTER TABLE `comparisons`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- AUTO_INCREMENT untuk tabel `criterias`
+-- AUTO_INCREMENT for table `criterias`
 --
 ALTER TABLE `criterias`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `failed_jobs`
+-- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `hasils`
+-- AUTO_INCREMENT for table `hasils`
 --
 ALTER TABLE `hasils`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=171;
 
 --
--- AUTO_INCREMENT untuk tabel `migrations`
+-- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT untuk tabel `sub_criterias`
+-- AUTO_INCREMENT for table `sub_criterias`
 --
 ALTER TABLE `sub_criterias`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `comparisons`
+-- Constraints for table `comparisons`
 --
 ALTER TABLE `comparisons`
   ADD CONSTRAINT `comparisons_ibfk_1` FOREIGN KEY (`tipe`) REFERENCES `alternatives` (`tipe`);
 
 --
--- Ketidakleluasaan untuk tabel `hasils`
+-- Constraints for table `hasils`
 --
 ALTER TABLE `hasils`
   ADD CONSTRAINT `hasils_ibfk_1` FOREIGN KEY (`tipe`) REFERENCES `alternatives` (`tipe`);
